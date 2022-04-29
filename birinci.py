909e48d4191a44e6c3337e76c9a0251b653199e6
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-04-29 13:26:05 +0400

    Update

---
+++ birinci.py
@@ -0,0 +1,10 @@
+# 1) İstifadəçidən aldığı yaş dəyərinə əsasən yetkinlik yaşına çatmış şəxsləri təyin edən skript
+
+print('Zəhmət olmasa yaşınızı qeyd edin:')
+a = input()
+print('Yaşınız,' + a)
+
+if a >= 18:
+    print("Sizə icazə var")
+else:
+    print("Sizə icazə yoxdur")
